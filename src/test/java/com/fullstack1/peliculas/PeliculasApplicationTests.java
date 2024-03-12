cd2cdd7826a030d0c7f8e558132426d2be1c0f53
Francisco Valdés <ecodisonante@gmail.com>
2024-03-12 20:49:07 -0300

    Creacion del proyecto spring boot

---
+++ src/test/java/com/fullstack1/peliculas/PeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack1.peliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
