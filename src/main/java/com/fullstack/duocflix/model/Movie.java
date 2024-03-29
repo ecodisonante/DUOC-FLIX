b373b17d0baa4f4c5007f994c5c95108fc0c5ca1
Francisco Valdés <ecodisonante@gmail.com>
2024-03-29 16:30:59 -0300

    Fix: cambio sinopsis tipo clob

--- src/main/java/com/fullstack/duocflix/model/Movie.java
+++ src/main/java/com/fullstack/duocflix/model/Movie.java
@@ -5,12 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "pelicula")
+@Table(name = "movie")
 public class Movie {
 
     @Id
@@ -25,6 +26,7 @@ public class Movie {
     private String director;
     @Column(name = "genre")
     private String genre;
+    @Lob
     @Column(name = "synopsis")
     private String synopsis;
 
