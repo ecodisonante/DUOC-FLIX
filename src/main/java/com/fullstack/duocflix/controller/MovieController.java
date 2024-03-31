1e2a508e5380bd26bb5b8dff70216ec8370a9bd3
Francisco Valdés <ecodisonante@gmail.com>
2024-03-31 14:47:36 -0300

    retornar  error en  formato json

--- src/main/java/com/fullstack/duocflix/controller/MovieController.java
+++ src/main/java/com/fullstack/duocflix/controller/MovieController.java
@@ -3,17 +3,18 @@ package com.fullstack.duocflix.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.fullstack.duocflix.dto.MovieDto;
+import com.fullstack.duocflix.dto.ExceptionResponse;
 import com.fullstack.duocflix.model.Movie;
 import com.fullstack.duocflix.service.DtoService;
 import com.fullstack.duocflix.service.IMovieService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/movies")
@@ -25,21 +26,27 @@ public class MovieController {
     DtoService _dto;
 
     @GetMapping
-    public List<MovieDto> getMovieList() {
+    public ResponseEntity<Object> getMovieList() {
         List<Movie> movies = _service.getAllMovies();
-        return movies.stream()
+
+        if (movies.isEmpty())
+            return ResponseEntity.ok(new ExceptionResponse(
+                    HttpStatus.OK.value(), "Aun no hay peliculas disponibles"));
+
+        return ResponseEntity.ok(movies.stream()
                 .map(movie -> _dto.toMovieDto(movie))
-                .toList();
+                .toList());
     }
 
     @GetMapping("/{id}")
-    public Movie getMovie(@PathVariable Long id) {
+    public ResponseEntity<Object> getMovie(@PathVariable Long id) {
         Optional<Movie> movie = _service.getMovieById(id);
 
-        if (movie.isPresent())
-            return movie.get();
-        else
-            return null;
+        if (!movie.isPresent())
+            return ResponseEntity.badRequest().body(new ExceptionResponse(
+                    HttpStatus.BAD_REQUEST.value(), "No existe la pelicula con el id " + id));
+
+        return ResponseEntity.ok(movie.get());
     }
 
 }
