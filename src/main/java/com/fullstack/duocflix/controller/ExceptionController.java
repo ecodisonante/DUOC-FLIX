95222e8fbc7c81e158bd0a65aa314dcdbc45ea27
Francisco Valdés <ecodisonante@gmail.com>
2024-03-31 14:48:14 -0300

    capturar rutas invalidas

---
+++ src/main/java/com/fullstack/duocflix/controller/ExceptionController.java
@@ -0,0 +1,20 @@
+package com.fullstack.duocflix.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fullstack.duocflix.dto.ExceptionResponse;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseEntity<Object> notFoundUri() {
+        return ResponseEntity.badRequest()
+                .body(new ExceptionResponse(HttpStatus.BAD_REQUEST.value(), "Ooops!! Creo que te perdiste."));
+    }
+}
